f86655b63d1063d208ef627f0104bee6bd1f5fc1
kathy49 <kathy49@ad.unc.edu>
2024-04-27 17:23:28 -0400

    add items table

--- server.js
+++ server.js
@@ -27,6 +27,25 @@ db.serialize(() => {
       FOREIGN KEY (user_id) REFERENCES users(id)
     )
   `);
+
+  db.run(`CREATE TABLE items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT
+);
+`);
+
+  db.run(`CREATE TABLE user_items (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  item_id INTEGER NOT NULL,
+  quantity INTEGER DEFAULT 1,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (item_id) REFERENCES items(id),
+  UNIQUE(user_id, item_id)
+);
+`)
 });
 
 app.use(bodyParser.json());
