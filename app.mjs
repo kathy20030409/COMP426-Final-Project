fc2e6107ad7f0d01dab932a20e1093896986cb0a
PanLu09 <panlu@email.unc.edu>
2024-04-29 17:43:30 -0400

    included secretkey.

--- app.mjs
+++ app.mjs
@@ -9,18 +9,6 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
-// const authenticateToken = (req, res, next) => {
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-//   if (token == null) return res.sendStatus(401);
-
-//   jwt.verify(token, SECRET_KEY, (err, user) => {
-//     if (err) return res.sendStatus(403);
-//     req.user = user;
-//     next();
-//   });
-// };
-
 app.post("/api/register", async (req, res) => {
   register(req, res);
 });
